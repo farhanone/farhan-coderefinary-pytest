5d23c0cf7e335b1096295840ff82bb66fa98675d
Farhan Rasheed <farhan.one@hotmail.com>
2021-03-17 10:37:25 +0100

    very first commit

---
+++ example.py
@@ -0,0 +1,14 @@
+import pytest
+#@pytest.mark.add
+#pytest -k add -v example.py 
+def add(a, b):
+    return a + b
+def sub(a, b):
+    return a - b
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add('space', 'ship') == 'spaceship'
+    
+def test_sub():
+    assert sub(2, 3) == -1
